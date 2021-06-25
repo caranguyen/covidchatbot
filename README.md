96678c53b23a3ee8eb8f60ebb8aea0dc982ef4b7
caranguyen <47336701+caranguyen@users.noreply.github.com>
2021-06-24 20:41:43 -0500

    commit with website

--- README.md
+++ README.md
@@ -2,24 +2,33 @@
 
 Python requirements setup: python 3.0+, pip
 
-This program is run and tested on Windows 10 with Anaconda Compiler. 
+This program is run and tested on Windows 10. 
 
 Optional to wrap the project in virtualenv: 
 <pre><code>C:\> python3 -m venv ./venv
 C:\> .\venv\Scripts\activate
 </code></pre>
 
+For Mac: 
+<pre><code>C:\> python3 -m venv ./venv
+C:\> .\venv\bin\activate
+</code></pre>
+
 To install Rasa:
 <pre><code>pip3 install rasa</code></pre>
-Then, download all files above
 
-Next, to run the current model:
+to see where are Rasa directory and files, run:
+<pre><code>rasa init</code></pre>
+
+Then, clone files in this repo in the same directory as venv folder. If you are in the correct Rasa directory, files in this repo will replace files created by rasa init.
+
+Next, train the model:
+<pre><code>rasa train</code></pre>
+
+To run the current model on command prompt:
 <pre><code>rasa shell</code></pre>
 
-The shell will prompt user input. Currently, here are the topics that chat bot can answer
-- success rate of 3 main vaccine providers
-- number of and time between dose of 3 main vaccine providers
-- covid demographic
-- who should not take vaccine
-- after care instruction
+The shell will prompt user input, then responses accordingly to the user input
 
+To run Rasa for website widget:
+<pre><code>rasa run -m models --enable-api --cors "*" </code></pre>
--- actions/actions.py
+++ actions/actions.py
@@ -7,12 +7,75 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
+# from typing import Any, Text, Dict, List, Union
+
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
+# from rasa_sdk.forms import FormAction
+
+# class VaccineSuggestion(FormAction):
+
+# 	def name(self):
+# 		return "vaccine_suggestion_form"
+
+# 	async def required_slots(
+# 		self,
+#         slots_mapped_in_domain: List[Text],
+#         dispatcher: "CollectingDispatcher",
+#         tracker: "Tracker",
+#         domain: "DomainDict",
+#     ) -> Optional[List[Text]]:
+#         return slots_mapped_in_domain
+
+# 	@staticmethod
+# 	def required_slots(tracker):
+
+# 		if tracker.get_slots("female_between_18_49") == True:
+# 			dispatcher.utter_message(text="CDC do not recommend one vaccine over the other. But CDD advices precausion to female between 18 and 49 before getting Janssen vaccine due to posible blood clots. Pfizer and Moderna vaccine are available alternatives.")
+# 			return ["female_between_18_49"]
+# 	 	else:
+# 	 		if tracker.get_slots("under_12") == True:
+# 	 			dispatcher.utter_message(text="CDC do not recommend one vaccine over the other. Currently, the available vaccines did not include clinical trials on people under the age of 12. Please contact your primary healthcare provider for more consults."
+# 	 		return []
+# 	 		if tracker.get_slots("between_12_and_18") == True:
+# 	 			dispatcher.utter_message(text="CDC do not recommend one vaccine over the other. Currently, Pfizer vaccine is the only available vaccine had people over 12 years old in its clinical trial with 95 percent effective."
+# 			return ["between_12_and_18"]
+# 			else:
+# 				if tracker.get_slots("two_dose_commitment") == False:
+# 	 			dispatcher.utter_message(text="CDC do not recommend one vaccine over the other. For 1 dose only, Janssen has the highest effective rate among 3 major vaccines with 66 percent."
+# 	 			return
+# 	 				else:
+# 		 					dispatcher.utter_message(text="CDC do not recommend one vaccine over the other. You can get any availabe vaccines: Pfizer, Moderna, and Janssen."		 	
+# 		 					return
+
+# 	def submit(
+# 		self,
+# 		dispatcher: CollectingDispatcher,
+# 		tracker: Tracker,
+# 		domain: Dict[Text, Any]
+# 	) -> List[Dict]:
+# 		return []
+
+# 	def slot_mapping(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+# 		return {
+# 			"female_between_18_49": [
+# 				self.from_intent(intent="affirm", value=True),
+# 				self.from_intent(intent="deny", value=False),
+# 			],
+# 			"under_12": [
+# 				self.from_intent(intent="affirm", value=True),
+# 				self.from_intent(intent="deny", value=False),
+# 			],
+# 			"between_12_and_18": [
+# 				self.from_intent(intent="affirm", value=True),
+# 				self.from_intent(intent="deny", value=False),
+# 			],
+# 			"two_dose_commitment": [
+# 				self.from_intent(intent="affirm", value=True),
+# 				self.from_intent(intent="deny", value=False),
+# 			]
+# 		}
+
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
